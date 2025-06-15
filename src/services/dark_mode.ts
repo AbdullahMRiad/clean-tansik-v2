278d86c1fc2761f2df911200e0ac375b3ab20e66
AbdullahMRiad <abdullahmriad@gmail.com>
2025-06-15 14:37:47 +0300

    Dark mode toggle now changes icon

--- src/services/dark_mode.ts
+++ src/services/dark_mode.ts
@@ -1,15 +1,21 @@
 // Dark mode logic
 
+import { darkModeToggle } from "../utils/dom";
+
 export var isDark: boolean = false;
 
 // Function to get dark mode (now only follows browser preference)
 export function getDarkMode() {
     isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
     document.documentElement.classList.toggle("dark", isDark);
+    if(!darkModeToggle) return;
+    darkModeToggle.innerHTML = isDark ? "light_mode" : "dark_mode";
 }
 
 // Function to toggle dark mode (manual override, no localStorage)
 export function setDarkMode() {
     isDark = !document.documentElement.classList.contains("dark");
     document.documentElement.classList.toggle("dark", isDark);
+    if(!darkModeToggle) return;
+    darkModeToggle.innerHTML = isDark ? "light_mode" : "dark_mode";
 }
